8860515877ec36291721b63e86aa5a1e8760eab7
zetaleo <alaverdi17@mail.ru>
2021-08-19 15:08:17 +0300

    Update main.js

--- melody/js/main.js
+++ melody/js/main.js
@@ -4,18 +4,20 @@ $(document).ready(function () {
   let counterUp = $('.counter-up');
   let counterDown = $('.counter-down');
   let usCurrentFloor;
+  // let iOS = navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  // let event = "click";
 
-  $('.home-image path').on("click", function () {  
+  $('.home-image path').on("click", function () {
+    $('.home-image path').removeClass('current-floor');
     currentFloor = $(this).attr('data-floor');
     $(".counter").text(currentFloor);
-    usCurrentFloor = currentFloor.toLocaleString('en-US', {
-      minimumIntegerDigits: 2,
-      useGrouping: false
-    });
-    $('.home-image path').removeClass('current-floor');
-    $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor');
-
+    // usCurrentFloor = currentFloor.toLocaleString('en-US', {
+    //   minimumIntegerDigits: 2,
+    //   useGrouping: false
+    // });
+    // $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor');
   });
+
   counterUp.on("click", function () {
     if (currentFloor < 18) {
       currentFloor++;
